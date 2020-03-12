f962788d41fe55b64d4e1d767da5ba811c15fe66
Ian Otto <ieo2@nau.edu>
2020-03-12 04:38:42 +0000

    Assignment 2 work stuffs

---
+++ Assignment2/Test2.cpp
@@ -0,0 +1,17 @@
+#include <deepstate/DeepState.hpp>
+//#include <stdio.h>
+//#include <stdlib.h>
+//extern "C" {
+//#include "c_printf/sources/c_printf.h"
+//}
+
+using namespace deepstate;
+
+TEST(CPrintF, PrintString){
+    size_t our_size = DeepState_SizeInRange(1,4096);
+    LOG(TRACE) << "String size = " << our_size;
+    // random string from deepstate
+    char* deepStateString = DeepState_CStrUpToLen(our_size);
+    // test copy function
+    ASSERT_EQ(1, 1);
+}
